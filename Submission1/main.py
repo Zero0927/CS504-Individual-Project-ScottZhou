88bb66b0606d3aec6e2024ee51ba6e6c48dfe9cd
Scott Zhou <zhouscott@hotmail.com>
2021-07-23 15:03:43 -0700

    Revert "Add another test case"
    This reverts commit 39a878d646e60d8f882500b76ec00d59e4caf670.

--- Submission1/main.py
+++ Submission1/main.py
@@ -3,8 +3,3 @@ from QuickSort import *
 array_to_be_sorted = [4,2,7,3,1,6,8,9]
 print("Original Array: ",array_to_be_sorted)
 print("Sorted Array: ",QuickSort(array_to_be_sorted))
-
-
-array_to_be_sorted = [1,3,9,6,8,5]
-print("Original Array: ",array_to_be_sorted)
-print("Sorted Array: ",QuickSort(array_to_be_sorted))
